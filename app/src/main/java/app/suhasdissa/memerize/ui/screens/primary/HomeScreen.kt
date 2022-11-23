c35aa3d6fb9dbf9216cdcd3d3772e7ba66bcce69
Suhas Dissanayake <suhasdissa@protonmail.com>
2022-11-23 16:17:07 +0530

    Add Copyright Headers

--- app/src/main/java/app/suhasdissa/memerize/ui/screens/primary/HomeScreen.kt
+++ app/src/main/java/app/suhasdissa/memerize/ui/screens/primary/HomeScreen.kt
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ Created By Suhas Dissanayake on 11/23/22, 4:16 PM
+ Copyright (c) 2022
+ https://github.com/SuhasDissa/
+ All Rights Reserved
+ ******************************************************************************/
+
 package app.suhasdissa.memerize.ui.screens.primary
 
 import androidx.compose.foundation.layout.Arrangement
