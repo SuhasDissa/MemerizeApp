15a4562f8b9e7ae9606d2369ab296236d01f0f20
Suhas Dissanayake <suhasdissa@protonmail.com>
2023-08-04 12:59:03 +0530

    Add title to differentiate subreddits and communities

--- app/src/main/java/app/suhasdissa/memerize/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/app/suhasdissa/memerize/ui/screens/home/HomeScreen.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -41,6 +43,9 @@ fun HomeScreen(
             .fillMaxSize()
             .padding(horizontal = 8.dp)
     ) {
+        item {
+            Text("Subreddits", style = MaterialTheme.typography.titleLarge)
+        }
         items(items = subreddits) {
             HighlightCard(
                 onClick = {
@@ -51,7 +56,9 @@ fun HomeScreen(
                 thumbnail_url = it.iconUrl
             )
         }
-
+        item {
+            Text("Lemmy Communities", style = MaterialTheme.typography.titleLarge)
+        }
         items(items = communities) {
             HighlightCard(
                 onClick = {
