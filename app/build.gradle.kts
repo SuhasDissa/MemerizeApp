34d17501a7cdb13f87bb346461ddbcf1e10a10b4
Suhas Dissanayake <suhasdissa@gmail.com>
2023-08-24 11:35:09 +0530

    Bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "app.suhasdissa.memerize"
         minSdk = 24
         targetSdk = 34
-        versionCode = 22
-        versionName = "2.2"
+        versionCode = 23
+        versionName = "2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
